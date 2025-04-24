bcbf5d9d9ea5ab4c83fe9476ad1c1f51eb99e431
Samyrjob <s.rjob@audencia.com>
2025-04-24 15:50:09 +0100

    successful solving for ng error import moduel store ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { AuthInterceptor } from './authentication/auth.interceptor';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { provideStore, StoreModule } from '@ngrx/store';
+import { appReducer } from './app.reducer';
+import { authReducer } from './authentication/auth.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
       provideRouter(routes),
       provideClientHydration(withEventReplay()), 
       provideHttpClient(withInterceptorsFromDi()), provideClientHydration(), 
+      provideStore(appReducer), // Provide the NgRx StoreModule with your reducers
 
       {
         provide: HTTP_INTERCEPTORS,
@@ -20,7 +24,12 @@ export const appConfig: ApplicationConfig = {
     {
       provide: JwtHelperService,
       useValue: new JwtHelperService(), // Create an instance of JwtHelperService
-    },
-    // Provide tokenGetter function for JWT configuration
+    }
+      //   // Provide the NgRx StoreModule with your reducers
+      // StoreModule.forRoot(appReducer) should be moved to an NgModule imports array
+      // StoreModule.forRoot({ auth: authReducer }), // Add this line to provide the auth reducer
+    // {
+      
+    // }
     
     ]};
